9cd5594bd31ef1b307761f521f5d9bdbeb66631c
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 15:14:40 -0300

    Create Aula_1.js

---
+++ Introdução JavaScript/Aula_1.js
@@ -0,0 +1,12 @@
+// Criação de variável em javascript
+
+const idade = 20
+const nome = 'Laura Valamiel'
+const curso = 'Sistema de Informação'
+let nota = 89.5
+nota = 90.5
+
+console.log('Minha idade: ', idade)
+console.log('Meu nome é: ', nome)
+console.log('Meu curso: ', curso)
+console.log('Minha nota é: ', nota)
