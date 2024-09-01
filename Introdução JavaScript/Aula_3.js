bf56ab5061c836212b64f96fb935908fc49abf9c
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 15:15:59 -0300

    Create Aula_3.js

---
+++ Introdução JavaScript/Aula_3.js
@@ -0,0 +1,31 @@
+//Operadores Aritmeticos: soma, subtração, divisão, multiplicação, potenciação, resto
+
+const primeiroNumero = 100
+const segundoNumero = 25
+const soma = primeiroNumero + segundoNumero
+const subtracao = primeiroNumero - segundoNumero
+const divisao = primeiroNumero / segundoNumero
+const multiplicacao = primeiroNumero * segundoNumero
+
+console.log('Primeiro número: ', primeiroNumero)
+console.log('Segundo número: ', segundoNumero)
+console.log('Resultado da soma: ', soma)
+console.log('Resultado da subtração: ', subtracao)
+console.log('Resultado da divisão: ', divisao)
+console.log('Resultado da multiplicação: ', multiplicacao)
+
+
+//Calcular área de um triângulo
+const base = 90
+const altura = 50
+const area = (base * altura)/2
+console.log('A área do triângulo é: ', area)
+
+//Potência e resto
+console.log('2 elevado a 10 é igual a: ', (2 **10))
+console.log('10 resto 2 é igual a: ', (10%2))
+
+//Contador não é constante
+let contador = 0
+contador++
+console.log('O valor do contador é: ', contador)
