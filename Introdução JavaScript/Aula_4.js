7ef935145639260cf717980b282f6dd0d24232f0
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 15:16:37 -0300

    Create Aula_4.js

---
+++ Introdução JavaScript/Aula_4.js
@@ -0,0 +1,32 @@
+//Usando Math
+
+const pi = Math.PI
+console.log('O valor de pi é: ', pi)
+
+const potencia = Math.pow(2, 10) //primeiro numero base, e segundo expoente
+console.log('O valor de 2 elevado a 10 é: ', potencia)
+
+const logaritimo = Math.log(10)
+console.log('O valor de log de 10 é: ', logaritimo)
+
+const minimo = Math.min(10, 15, 20)
+console.log('O menor número entre 10, 15 e 20 é: ', minimo)
+
+const maximo = Math.max(20, 40, 85, 12)
+console.log('O maior  número entre 20, 40, 85, 12 é: ', maximo)
+
+//Se não multiplicar por nada, o Math.random retorna um número de 0 a 1
+const aleatorio = Math.random() * 100
+console.log('Número aleatório entre 0 e 100: ', aleatorio)
+
+const raizQuadrada = Math.sqrt(100)
+console.log('Raiz quadrada de 100 é: ', raizQuadrada)
+
+const seno = Math.sin(60)
+console.log('Seno de 60: ', seno)
+
+const cosseno = Math.cos(60)
+console.log('Cosseno de 60: ', cosseno)
+
+const tangente = Math.tan(90)
+console.log('Tangente de 90: ',  tangente)
