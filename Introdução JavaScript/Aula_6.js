830704173edd5283e85f9cb1840c2096ef4c9d15
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 15:18:03 -0300

    Create Aula_6.js

---
+++ Introdução JavaScript/Aula_6.js
@@ -0,0 +1,9 @@
+//Calcular a área do círculo
+
+const pi = Math.PI
+const raio = 10
+const area = pi * Math.pow(raio, 2)//area = pi * raio *raio
+console.log('A área do círculo é: ', area.toFixed(2))
+
+//saída de outra forma
+console.log(`Area do círculo: ${pi.toFixed(2)} * ${raio} * ${raio} = ${area.toFixed(2)}`)
