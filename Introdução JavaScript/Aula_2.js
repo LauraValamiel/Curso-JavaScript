ead5a9e26989314d9c8d5b94b38c6cebb18b261b
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 15:15:18 -0300

    Create Aula_2.js

---
+++ Introdução JavaScript/Aula_2.js
@@ -0,0 +1,17 @@
+//Tipos de dados em Javascript
+
+let nome = 'Laura Valamiel' //string
+//nome = 20 //number
+console.log('Tipo de dados em nome: ', typeof nome)
+
+let idade = 20 //number
+console.log('Tipo de dados em idade: ', typeof idade)
+
+let nota = 85.9 //number, em javascript nao tem diferenca entre float e inteiro
+console.log('Tipo de dados em nota: ', typeof nota)
+
+let aprovado = true //boolean
+console.log('Tipo de dados em aprovado: ', typeof aprovado)
+
+let curso //undefined
+console.log('Tipo de dados em curso: ', typeof curso)
