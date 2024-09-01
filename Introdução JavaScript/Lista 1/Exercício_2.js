61425246bc506e1f33da088f3f24fe952ba0900f
Laura Valamiel <140740714+LauraValamiel@users.noreply.github.com>
2024-09-01 16:14:27 -0300

    Create Exercício_2.js

---
+++ Introdução JavaScript/Lista 1/Exercício_2.js
@@ -0,0 +1,10 @@
+parseInt()
+
+const numAleatorio = parseInt(Math.random() * 100)
+
+const antecessor = numAleatorio - 1
+const sucessor = numAleatorio + 1
+
+console.log("O número aleatótrio entre 0 e 100 é: ", numAleatorio)
+console.log("Seu antecessor: ", antecessor)
+console.log("Seu sucessor: ", sucessor)
